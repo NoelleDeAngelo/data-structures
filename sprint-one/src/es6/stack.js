11532ea4a9d259caa6df40af3b8030bd57b58216
Noelle DeAngelo <NoelleDeAngelo@gmail.com>
2021-03-11 15:51:45 -0800

    Add class of Queue and stack in es6 pattern

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,24 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
   constructor() {
+    this.count = 0;
+    this.storage = {};
   }
 
+  push(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  pop() {
+    var popped = this.storage[this.count - 1];
+    if (this.count > 0) {
+      this.count -= 1;
+    }
+    return popped;
+  }
+
+  size() {
+    return this.count;
+  }
 }
