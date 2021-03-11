0d14b754726619530d9a13583e5e0ba2c3c93fb8
Noelle DeAngelo <NoelleDeAngelo@gmail.com>
2021-03-11 11:01:13 -0800

    Add methods for queue and Stack in protoypal patern

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,9 +1,31 @@
-var Stack = function() {
+// Hey! Rewrite in the new style. Your code will wind up looking very similar,
+// but try not not reference your old code in writing the new style.
 
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Stack = function() {
+  var someStack = Object.create(stackMethods);
+  someStack.storage = {};
+  someStack.count = 0;
+  return someStack;
 };
 
 var stackMethods = {};
+
+
+stackMethods.push = function (value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
+
+stackMethods.pop = function () {
+  var popped = this.storage[this.count - 1];
+  if (this.count > 0) {
+    this.count -= 1;
+  }
+  return popped;
+};
+
+stackMethods.size = function () {
+  return this.count;
+};
 
 
