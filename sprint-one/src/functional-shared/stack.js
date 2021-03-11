fdba3881e8e063c40522d33a666a7fb87a3a5d63
Noelle DeAngelo <NoelleDeAngelo@gmail.com>
2021-03-11 10:28:11 -0800

    Add push, pop, and size methods for functional-shared Stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,29 @@
+// Hey! Rewrite in the new style. Your code will wind up looking very similar,
+// but try not not reference your old code in writing the new style.
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someStack = {};
+  someStack.storage = {};
+  someStack.count = 0;
+  _.extend(someStack, stackMethods);
+  return someStack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
 
+stackMethods.pop = function() {
+  var popped = this.storage[this.count - 1];
+  if (this.count > 0) {
+    this.count -= 1;
+  }
+
+  return popped;
+};
+
+stackMethods.size = function () {
+  return this.count;
+};
