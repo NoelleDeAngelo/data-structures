845b895af8841d08b8c450d294c5ebabeed37b67
Noelle DeAngelo <NoelleDeAngelo@gmail.com>
2021-03-11 17:35:03 -0800

    Pass all preexisting tests for LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,44 @@
+
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.count = 0;
 
   list.addToTail = function(value) {
+    var addedNode = Node(value);
+    list.tail = addedNode;
+    if (list.head === null) {
+      list.head = addedNode;
+    } else {
+      currentNode = list.head;
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = addedNode;
+    }
+
   };
 
   list.removeHead = function() {
+    var removed = list.head.value;
+    list.head = list.head.next;
+    return removed;
+
   };
 
   list.contains = function(target) {
+    currentNode = list.head;
+    while (currentNode.next) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    if (currentNode.value === target ) {
+      return true;
+    }
+    return false;
   };
 
   return list;
