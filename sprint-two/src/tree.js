53416caeb1715db6aae7df03e16a6d7068af12f9
Noelle DeAngelo <NoelleDeAngelo@gmail.com>
2021-03-11 22:01:06 -0800

    Add addchild method to Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,11 @@
+
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  _.extend(newTree, treeMethods);
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = []; // fix me
 
   return newTree;
 };
@@ -11,9 +13,27 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
+
 };
 
 treeMethods.contains = function(target) {
+  currentNode = this;
+  if (currentNode.value === target) {
+    return true;
+  }
+  var searchChildren = function (currentNode) {
+    for (var i = 0; i < currentNode.children.length; i++) {
+      var child = currentNode.children[i];
+      if (child.value === target) {
+        return true;
+      }
+      if (child.children.length > 0 ) {
+        searchChildren(child);
+      }
+    }
+  };
+  return false;
 };
 
 
